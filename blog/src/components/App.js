d8999e343c04c5639e5cf3758fa1e22f689f75ca
Yulieth Lubo <yuliethdotcom@gmail.com>
2019-07-10 12:17:27 -0500

    Creating a not functional component

--- blog/src/components/App.js
+++ blog/src/components/App.js
@@ -1,7 +1,54 @@
 import React from 'react';
 
 const App = () => {
-return('Hola')
+  const putRows = () => [
+    <tr>
+      <td>
+        Yulieth
+    </td>
+      <td>
+        yuliethdotcom@gmail.com
+    </td>
+      <td>
+        yulieth.com
+    </td>
+    </tr>, 
+    <tr>
+    <td>
+      Marco
+    </td>
+    <td>
+      Marco@gmail.com
+    </td>
+    <td>
+      marco.com
+    </td>
+  </tr>
+
+  ];
+
+  return (
+    <div className="margen">
+    <table className="tabla">
+      <thead>
+        <tr>
+          <th>
+            Nombre
+          </th>
+          <th>
+            Correo
+          </th>
+          <th>
+            Enlace
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+          {putRows()}
+      </tbody>
+      </table>
+      </div>
+  );
 }
 
 export default App;
